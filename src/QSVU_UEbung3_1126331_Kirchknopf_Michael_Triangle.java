27022069dd8c240a9d0b7709842781d417b5fb05
MichiK91 <scr4ever@hotmail.com>
2013-12-14 16:03:39 +0100

    init

---
+++ src/QSVU_UEbung3_1126331_Kirchknopf_Michael_Triangle.java
@@ -0,0 +1,4 @@
+
+public class QSVU_UEbung3_1126331_Kirchknopf_Michael_Triangle {
+
+}
---
+++ src/QSVU_UEbung3_1126331_Kirchknopf_Michael_TriangleTests.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import java.util.Stack;
+
+import org.junit.Test;
+
+public class QSVU_UEbung3_1126331_Kirchknopf_Michael_TriangleTests {
+	
+	public QSVU_UEbung3_1126331_Kirchknopf_Michael_TriangleTests() {
+		
+	}
+
+	@Test
+	public void test() {
+	}
+}
