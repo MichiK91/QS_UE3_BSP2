a2a9d75c76d8e047b694a09b8797626dc87eab8e
MichiK91 <scr4ever@hotmail.com>
2013-12-15 12:47:20 +0100

    hypotenuse tests

--- src/QSVU_UEbung3_1126331_Kirchknopf_Michael_Triangle.java
+++ src/QSVU_UEbung3_1126331_Kirchknopf_Michael_Triangle.java
@@ -1,4 +1,23 @@
+import java.security.InvalidParameterException;
+
 
 public class QSVU_UEbung3_1126331_Kirchknopf_Michael_Triangle {
+	
+	public QSVU_UEbung3_1126331_Kirchknopf_Michael_Triangle(){
+		
+	}
+	
+	public double calcHypotenuse(double k1, double k2) throws InvalidParameterException {
+		return 0;
+	}
+	
+	public double calcWinkel(double w)throws InvalidParameterException {
+		return 0;
+	}
+	
+	public double calcFlaeche(double k1, double k2) throws InvalidParameterException {
+		return 0;
+	}
+	
 
 }
--- src/QSVU_UEbung3_1126331_Kirchknopf_Michael_TriangleTests.java
+++ src/QSVU_UEbung3_1126331_Kirchknopf_Michael_TriangleTests.java
@@ -1,16 +1,41 @@
 import static org.junit.Assert.*;
 
+import java.security.InvalidParameterException;
 import java.util.Stack;
 
 import org.junit.Test;
 
 public class QSVU_UEbung3_1126331_Kirchknopf_Michael_TriangleTests {
 	
+	QSVU_UEbung3_1126331_Kirchknopf_Michael_Triangle triangle;
+	
 	public QSVU_UEbung3_1126331_Kirchknopf_Michael_TriangleTests() {
-		
+		new QSVU_UEbung3_1126331_Kirchknopf_Michael_Triangle();
 	}
 
 	@Test
-	public void test() {
+	public void testHypotenuseValidTriangle() {
+		Double h = triangle.calcHypotenuse(3,4);
+		assertEquals(h,new Double(5));
+	}
+	
+	@Test
+	public void testHypotenuseInvalidTriangle() {
+		try{
+			double h = triangle.calcHypotenuse(3,3);
+			fail("Should not happen");
+		} catch(InvalidParameterException e){
+			assertEquals("Falsche Parameter", e.getMessage());			
+		}
+	}
+	
+	@Test
+	public void testHypotenuseInvalidParameter() {
+		try{
+			double h = triangle.calcHypotenuse(-3,3);
+			fail("Should not happen");
+		} catch(InvalidParameterException e){
+			assertEquals("Negativer Parameter", e.getMessage());			
+		}
 	}
 }
